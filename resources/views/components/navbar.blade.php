18a15007fb5a490e2518fe0120a231acd468ff48
KaisAbiyyi <kaisabiyyi2@gmail.com>
2023-09-13 10:05:47 +0700

    navbarDropdown

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,17 +3,47 @@
         <a href="/" class="text-3xl font-bold uppercase text-slate-100">E-Commerce</a>
         <form class="flex flex-grow">
             <input type="search" name="search" id="search" class="flex-grow px-4 py-2 outline-none bg-slate-100 focus:outline-2 focus:outline-offset-2 focus:outline-slate-100">
-            <button type="submit" class="px-4 py-2 font-bold text-red-500 uppercase duration-200 bg-slate-100 hover:bg-orange-500 hover:text-slate-100">Search</button>
+            <button type="submit" class="px-4 py-2 font-bold uppercase duration-200 text-slate-600 bg-slate-100 hover:bg-slate-200">Search</button>
         </form>
-        <div class="flex gap-4">
+        <div class="flex">
             @guest
-                <a href="{{ route('register.get') }}" class="px-4 py-2 font-bold text-red-500 uppercase duration-200 bg-slate-100 hover:bg-orange-500 hover:text-slate-100">Register</a>
-                <a href="{{ route('login.get') }}" class="px-4 py-2 font-bold text-red-500 uppercase duration-200 bg-slate-100 hover:bg-orange-500 hover:text-slate-100">Log In</a>
+                <a href="{{ route('register.get') }}" class="px-4 py-2 font-bold uppercase duration-200 text-slate-600 bg-slate-100 hover:bg-slate-200">Register</a>
+                <a href="{{ route('login.get') }}" class="px-4 py-2 font-bold uppercase duration-200 text-slate-600 bg-slate-100 hover:bg-slate-200">Log In</a>
 
             @endguest
             @auth
-                <a href={{ route('logout') }} class="px-4 py-2 font-bold text-red-500 uppercase duration-200 bg-slate-100 hover:bg-orange-500 hover:text-slate-100">Log Out</a>
+                <div class="relative flex flex-col">
+                    <button class="h-full px-4 text-2xl font-bold uppercase duration-200 text-slate-600 bg-slate-100 hover:bg-slate-200"
+                        id="mainNavProfileDropdownToggler">{{ Auth::user()->name[0] }}</button>
+                    <div class="absolute right-0 flex flex-col gap-2 p-2 translate-y-2 top-full bg-slate-100 drop-shadow-sm" id="mainNavProfileDropdown">
+                        <a href="" class="w-full px-2 py-1 text-sm font-semibold duration-200 whitespace-nowrap text-slate-600 hover:bg-slate-200">Profile</a>
+                        <a href="{{ route('logout') }}" class="w-full px-2 py-1 text-sm font-semibold duration-200 whitespace-nowrap text-slate-600 hover:bg-slate-200">Log Out</a>
+                    </div>
+                </div>
             @endauth
         </div>
     </div>
 </div>
+
+<script type="module">
+    $(document).ready(() => {
+        $("#mainNavProfileDropdown").slideUp()
+        let state = 0
+        $("#mainNavProfileDropdownToggler").click(() => {
+            if (state === 0) {
+                $("#mainNavProfileDropdown").slideDown(200)
+                state = 1
+            } else {
+                $("#mainNavProfileDropdown").slideUp(200)
+                state = 0
+            }
+        })
+
+        $(window).click((e) => {
+            if (e.target.id !== 'mainNavProfileDropdown' && e.target.id !== 'mainNavProfileDropdownToggler') {
+                state = 0
+                $("#mainNavProfileDropdown").slideUp(200)
+            }
+        })
+    })
+</script>
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Simple E-commerce @yield('title')</title>
 </head>
 
 <body class="bg-slate-200">
     @include('components.navbar')
-    <div class="container pt-4 mx-auto flex flex-col">
+    <div class="container flex flex-col pt-4 mx-auto">
         @yield('content')
     </div>
 </body>
